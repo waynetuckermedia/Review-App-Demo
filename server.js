1be7a4aa1186b1f43b58a480b609fbf45abb94c7
Wayne <waynetuckermedia@gmail.com>
2022-06-28 11:23:38 -0400

    added gitignore for modules

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express()
-const cors require('cors')
+const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
